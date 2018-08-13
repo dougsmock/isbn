375e9b7252df575fa023f7c09180a63f2d89652f
Your Name <you@example.com>
2018-08-13 12:23:41 -0400

    Stuck on passing functions. Again.

--- isbn_numbers.rb
+++ isbn_numbers.rb
@@ -4,7 +4,7 @@ def book_long(booknum) #changes string to array of one char each
   book_arr.delete_if {|bookn| bookn == " "} #removes spaces
   book_arr.delete_if {|bookn| bookn == "-"} #removes hyphens
   p book_arr
-  if book_arr.size == 13 || book_arr.size == 10
+  if book_arr.length == 13 || book_arr.length == 10
     p "Entry is correct length."
   else
     p "Entry invalid; not correct length."
@@ -13,19 +13,20 @@ def book_long(booknum) #changes string to array of one char each
   book_arr.length
 end
 
-def invalid_char(book_arr)
+def invalid_char_ten(book_arr)
+  p book_arr
   alpha1 = ('A'..'W').to_a
   alpha2 = ('Y'..'w').to_a
   alpha3 = ('y'..'z').to_a
   char1 = ('!'..',').to_a
   char2 = ('.'..'/').to_a
   char3 = (':'..'?').to_a
-  invalid_char = alpha1 + alpha2 + alpha3 + char1 + char2 + char3
-
+  bad_char = alpha1 + alpha2 + alpha3 + char1 + char2 + char3
+  p bad_char
   i = 0
     while i < book_arr.length do
-      p book_arr[i]
-      if book_arr[i].include?(invalid_char) #find correct method
+      p book_arr.length
+      if book_arr[i].include?(bad_char)
         p "Entry invalid; illegal characters."
         exit!
       else
@@ -36,9 +37,7 @@ def invalid_char(book_arr)
     book_arr
 end
 
-
-
-booknum = "893370-898-912 2" # right length, wrong variable
+booknum = "22345678FX" # 10 char correct
 book_long(booknum) # sends booknum to function
 
 
--- isbn_tdd.rb
+++ isbn_tdd.rb
@@ -27,10 +27,10 @@ def test_string_no_with_hyphens_and_spaces()
   assert_equal(13, book_long(book_arr))
 end
 
-# def test_if_letters_are_invalid # tested in both 10 and 13 chars
-#   booknum = "89337898123ff"
-#   assert_equal(false, booknum)
-# end
+def test_if_letters_are_valid_in_10_chars
+  book_arr = "12345678FX"
+  assert_equal(true, invalid_char_ten(book_arr))
+end
 
 
 
