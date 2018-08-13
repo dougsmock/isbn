b0e6904b94c8a8218a74d58cf0024fd717f1caa1
Your Name <you@example.com>
2018-08-13 10:53:45 -0400

    Tested for number length; illegal characters are next.

--- isbn_numbers.rb
+++ isbn_numbers.rb
@@ -1,31 +1,55 @@
-def book_arr(booknum)
-  booknum.split("")
-  p booknum
+def book_long(booknum) #changes string to array of one char each
+  book_arr = booknum.each_char.to_a
+  p book_arr
+  book_arr.delete_if {|bookn| bookn == " "} #removes spaces
+  book_arr.delete_if {|bookn| bookn == "-"} #removes hyphens
+  p book_arr
+  if book_arr.size == 13 || book_arr.size == 10
+    p "Entry is correct length."
+  else
+    p "Entry invalid; not correct length."
+    exit!
+  end
+  book_arr.length
+end
 
+def invalid_char(book_arr)
+  alpha1 = ('A'..'W').to_a
+  alpha2 = ('Y'..'w').to_a
+  alpha3 = ('y'..'z').to_a
+  char1 = ('!'..',').to_a
+  char2 = ('.'..'/').to_a
+  char3 = (':'..'?').to_a
+  invalid_char = alpha1 + alpha2 + alpha3 + char1 + char2 + char3
+
+  i = 0
+    while i < book_arr.length do
+      p book_arr[i]
+      if book_arr[i].include?(invalid_char) #find correct method
+        p "Entry invalid; illegal characters."
+        exit!
+      else
+        i += i + 1
+      end
+    end
+    p book_arr
+    book_arr
 end
 
 
+
+booknum = "893370-898-912 2" # right length, wrong variable
+book_long(booknum) # sends booknum to function
+
+
+
+
+
+
+
+
+
 #
-# def book_sz(booknum)
-#   p booknum
-#   p booknum.size
-#   if booknum.size != 12 && booknum.size != 9
-#     p "Number is invalid."
-#   end
-#   booknum
+# def isbn_numeric(book_arr)
+# stringer = "" # knock this out
 # end
-#
-
-
-
-
-
-
-booknum = "89337 898 9"
-book_arr(booknum)
-
-
-
-
-# book_sz(booknum)
-#booknum returned as entered
--- isbn_tdd.rb
+++ isbn_tdd.rb
@@ -7,20 +7,31 @@ def test_assert_1_equals_1
   assert_equal(1, 1)
 end
 
-# def test_is_string_all_numeric_12
-#   booknum = "893378989090"
-#   assert_equal(12, booknum.size)
-# end
-#
-# def test_is_string_all_numeric_9 #both tests cannot pass at same time with same string
-#   booknum = "893378989"
-#   assert_equal(9, booknum.size)
+def test_is_string_13_char
+  booknum = "8933789896890"
+  assert_equal(13, booknum.size)
+end
+
+# def test_is_string_10_char #above test and this test cannot pass at same time with same string
+#   booknum = "8933789896890"
+#   assert_equal(10, booknum.size)
 # end
 
-def test_string_no_with_spaces
-  booknum = "89 337 8989"
-  assert_equal(9, booknum.size)
+def test_string_no_without_spaces
+  booknum = "8933789896890"
+  assert_equal(13, booknum.size)
 end
+
+def test_string_no_with_hyphens_and_spaces()
+  book_arr = "89337-8989-68 90"
+  assert_equal(13, book_long(book_arr))
+end
+
+# def test_if_letters_are_invalid # tested in both 10 and 13 chars
+#   booknum = "89337898123ff"
+#   assert_equal(false, booknum)
+# end
+
 
 
 
