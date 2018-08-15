3372feaddb61c7a4c205aa56b59e9fe772313fde
Your Name <you@example.com>
2018-08-15 16:52:25 -0400

    trying to finish ISBN 13

--- isbn_numbers.rb
+++ isbn_numbers.rb
@@ -1,126 +1,170 @@
-def book_long(booknum) #changes string to array of one char each
-  book_arr = booknum.each_char.to_a
-  # p book_arr
-  book_arr.delete_if {|bookn| bookn == " "} #removes spaces
-  book_arr.delete_if {|bookn| bookn == "-"} #removes hyphens
-  # p book_arr
-  if book_arr.length == 13 || book_arr.length == 10
-    p "Entry is correct length."
-  else
-    p "Entry invalid; not correct length."
-    exit!
-  end
-  book_arr
-end
-
-def invalid_char_ten(booknum)
-  book_arr = book_long(booknum)
-  # p book_arr
-  alpha1 = ('A'..'W').to_a
-  alpha2 = ('Y'..'w').to_a
-  alpha3 = ('y'..'z').to_a
-  char1 = ('!'..',').to_a
-  char2 = ('.'..'/').to_a
-  char3 = (':'..'?').to_a
-  bad_char = alpha1 + alpha2 + alpha3 + char1 + char2 + char3
-  p bad_char
-
-  i = 0
-    while i < book_arr.length
-      j = 0
-      while j < bad_char.length
-        if book_arr[i] == bad_char[j]
-          p "Entry invalid; illegal character."
-          exit!
-        end
-        # p "jay #{j}"
-        j += 1
-      end
-      # p "eye #{i}"
-      i += 1
-    end
-    book_arr
-end
-
-def xposition_ten(booknum)
-  book_arr = invalid_char_ten(booknum)
-  # p book_arr
-  i = 0
-  while i < 9
-    if booknum[i] == "X" || booknum == "x"
-      p "Invalid number; X in improper position"
-      exit!
-    end
-    i += 1
-  end
-  book_arr
-end
-
-def math_ten(booknum)
-  book_arr = xposition_ten(booknum)
-
-  book_int = []
-  j = 0
-  while j < 9
-    book_int[j] = book_arr[j].to_i
-    j += 1
-  end
-
-  book_int << book_arr[9]
-  # p book_int
-
-  checkmath = (book_int[0] * 1) + (book_int[1] * 2) + (book_int[2] * 3) + (book_int[3] * 4) + (book_int[4] * 5) + (book_int[5] * 6) + (book_int[6] * 7) + (book_int[7] * 8) + (book_int[8] * 9)
-  checkdig_check = checkmath % 11
-
-  # p checkmath
-  # p checkdig_check
-
-  if (checkdig_check == 10 && book_arr[9] == "X") || (checkdig_check == 10 && book_arr[9] == "x")
-    p "Number is valid!"
-  elsif checkdig_check == book_arr[9]
-    p "Number is valid!"
-  else
-    p "Number is invalid."
-  end
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-booknum = "1234-5678 9X"
-book_long(booknum) # sends booknum to function
-invalid_char_ten(booknum)
-xposition_ten(booknum)
-math_ten(booknum)
-
-
-
-
-
-
-
-
-
-
-
-# this is merely practice on nested loops
-# i = 0
+# def book_long(booknum) #changes string to array of one char each; eliminates hyphens and spacesvalidates length
+#   p "1 book long called."
+#   book_arr = booknum.each_char.to_a
+#   book_arr.delete_if {|bookn| bookn == " "} #removes spaces
+#   book_arr.delete_if {|bookn| bookn == "-"} #removes hyphens
+#   if book_arr.length == 13 || book_arr.length == 10
+#     p "Entry is correct length."
+#   else
+#     p "Entry invalid; not correct length."
+#     exit!
+#   end
+#   book_arr
+# end
 #
-# while i < 3 do
+# def invalid_char(booknum)
+#   p "2/ invalid char called"
+#   alpha1 = ('A'..'W').to_a
+#   alpha2 = ('Y'..'w').to_a
+#   alpha3 = ('y'..'z').to_a
+#   char1 = ('!'..',').to_a
+#   char2 = ('.'..'/').to_a
+#   char3 = (':'..'?').to_a
+#   bad_char = alpha1 + alpha2 + alpha3 + char1 + char2 + char3
+#
+#   i = 0
+#     while i < booknum.length
+#       j = 0
+#       while j < bad_char.length
+#         if booknum[i] == bad_char[j]
+#           p "Entry invalid; illegal character."
+#           exit!
+#         end
+#         j += 1
+#       end
+#       i += 1
+#     end
+#     booknum
+# end
+#
+# def ten_or_thirteen(booknum) # picking 10 or 13 numbers
+#   p "3/ ten or 13 called"
+#   if booknum.length == 13
+#     math_13(booknum)
+#   elsif booknum.length == 10
+#     xposition_ten(booknum)
+#   end
+#   booknum
+# end
+#
+# def xposition_ten(booknum)
+#   p "4/ xposition_ten called"
+#   # book_arr = ten_or_thirteen(booknum)
+#   p booknum
+#   i = 0
+#   while i < 9
+#     if booknum[i] == "X" || booknum == "x"
+#       p "Invalid number; X in improper position"
+#       exit!
+#     end
+#     i += 1
+#   end
+#   booknum
+# end
+#
+# def math_ten(booknum) #function to do 10-number math
+#   p "5/ math 10 called"
+#   book_arr = xposition_ten(booknum)
+#
+#   book_int = []
 #   j = 0
-#   while j < 10 do
-#     puts "jay #{j}"
+#   while j < 9
+#     book_int[j] = book_arr[j].to_i
 #     j += 1
 #   end
-#   puts "eye #{i}"
-#   i += 1
+#
+#   book_int << book_arr[9]
+#   # p book_int
+#
+#   checkmath = (book_int[0] * 1) + (book_int[1] * 2) + (book_int[2] * 3) + (book_int[3] * 4) + (book_int[4] * 5) + (book_int[5] * 6) + (book_int[6] * 7) + (book_int[7] * 8) + (book_int[8] * 9)
+#   checkdig_check = checkmath % 11
+#
+#   # p checkmath
+#   # p checkdig_check
+#
+#   if (checkdig_check == 10 && book_arr[9] == "X") || (checkdig_check == 10 && book_arr[9] == "x")
+#     p "Number is valid!"
+#   elsif checkdig_check == book_arr[9]
+#     p "Number is valid!"
+#   else
+#     p "Number is invalid."
+#   end
 # end
+#
+# def math_13(booknum) #function to do 13-number math
+#   p "6/ math 13 called."
+#
+#
+#
+#
+# end
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# booknum = "1234-5678-9X"
+# p booknum
+# book_long(booknum) # sends booknum to function
+# invalid_char(booknum) # checks for invalid char in 10 or 13
+# ten_or_thirteen(booknum) # checks whether 10 or 13 characters
+
+
+# Refactoring here ////////////////////////////
+
+
+def check_length(isbn)
+  isbn.length == 10 || isbn.length == 13
+end
+
+def check_isbn(isbn)
+  arr = isbn.split("")
+  arr.delete_if {|c| c==" " || c=="-"}
+
+  check_length(arr) == true
+
+  isbn_sum = 0
+  isbn_check = arr.pop
+  if isbn_check.downcase == "x"
+    isbn_check = 10
+  else
+    isbn_check = isbn_check.to_i
+  end
+
+  arr.each_with_index do |v, i|
+    unless v.to_i.to_s == v
+      return false
+    end
+  isbn_sum += v.to_i * (i+1)
+  end
+
+  if isbn_sum % 11 == isbn_check
+    return true
+  else
+    return false
+  end
+end
+
+
+def check_isbn_13(isbn)
+    i = 0
+  while i < 13 do
+    isbn[i].to_i
+    i += 1
+  end
+
+  isbn_sum = isbn[0] + (isbn[1] * 3) + isbn[2] + (isbn[3] * 3) + isbn[4] + (isbn[5] * 3) + isbn[6] + (isbn[7] * 3) + isbn[8] + (isbn[9] * 3) + isbn[10] + (isbn[11] * 3)
+
+  isbn_mod = isbn_sum % 10
+  isbn_digit = 10 - isbn_mod
+
+  isbn_digit == isbn[12]
+
+end
--- isbn_tdd.rb
+++ isbn_tdd.rb
@@ -3,43 +3,105 @@ require_relative "isbn_numbers.rb"
 
 class HashSize < Minitest::Test
 
-def test_assert_1_equals_1
-  assert_equal(1, 1)
-end
-
-def test_is_string_13_char
-  booknum = "8933789896890"
-  assert_equal(13, booknum.size)
-end
-
-# def test_is_string_10_char #above test and this test cannot pass at same time with same string
+# def test_assert_1_equals_1
+#   assert_equal(1, 1)
+# end
+#
+# def test_is_string_13_char
 #   booknum = "8933789896890"
-#   assert_equal(10, booknum.size)
+#   assert_equal(13, booknum.size)
+# end
+#
+# # def test_is_string_10_char #above test and this test cannot pass at same time with same string
+# #   booknum = "8933789896890"
+# #   assert_equal(10, booknum.size)
+# # end
+#
+# def test_string_no_without_spaces
+#   booknum = "8933789896890"
+#   assert_equal(13, booknum.size)
+# end
+#
+# def test_string_no_with_hyphens_and_spaces() #... test deals with 13-char numbers
+#   book_arr = "89337-8989-68 90"
+#   assert_equal(13, book_long(book_arr))
+# end
+#
+# def test_if_letters_are_invalid_in_10_chars #... passed with weird failure
+#   book_arr = "12-3456 78-9X"
+#   assert_equal(true, invalid_char_ten(book_arr))
+# end
+#
+# def test_if_x_is_in_right_place
+#   booknum = "12-3456 78-9X"
+#   assert_equal("X is in right place!", xposition_ten(booknum))
+# end
+#
+# # def test_if_x_is_in_wrong_place #draws an invalid message, no count of failures, etc.
+# #   booknum = "12-X456 78-93"
+# #   assert_equal("X is in the right place!", xposition_ten(booknum))
+# # end
+#
+# def test_if_booknum_is_valid_or_not
+#   booknum = "1234 5678 9X"
+#   assert_equal("Number is valid!", math_ten(booknum))
+# end
+#
+# def test_that_booknum_is_invalid
+#   booknum = "1234 5678 90"
+#   assert_equal("Number is invalid.", math_ten(booknum))
+# end
+#
+# def test_that_booknum_is_invalid_by_X_position
+#   booknum = "1234 X678 90"
+#   assert_equal("Invalid number; X in improper position.", math_ten(booknum))
+# end
+#
+# def test_that_booknum_is_invalid_by_another_letter
+#   booknum = "1234 0678 9090 8"
+#   assert_equal("Entry invalid; illegal character.", invalid_char_ten(booknum))
 # end
 
-def test_string_no_without_spaces
-  booknum = "8933789896890"
-  assert_equal(13, booknum.size)
+#refactor ///////////////
+def test_if_length_is_10_with_spaces
+  assert_equal(true, check_isbn("047195 8697"))
 end
 
-def test_string_no_with_hyphens_and_spaces() #... test deals with 13-char numbers
-  book_arr = "89337-8989-68 90"
-  assert_equal(13, book_long(book_arr))
+def test_if_10_true
+  assert_equal(true, check_length("1234567890"))
 end
 
-def test_if_letters_are_invalid_in_10_chars #... passed with weird failure
-  book_arr = "12-3456 78-9X"
-  assert_equal(true, invalid_char_ten(book_arr))
+def test_if_bad_char_invalid
+  assert_equal(false, check_isbn("0471a58697"))
 end
 
-def test_if_x_is_in_right_place
-  booknum = "12-3456 78-9X"
-  assert_equal("Valid 10-character number!", xposition_ten(booknum))
+def test_if_false_if_not_10
+  assert_equal(false, check_length("12345678"))
 end
 
-def test_if_x_is_in_wrong_place
-  booknum = "12-X456 78-93"
-  assert_equal("Valid 10-character number!", xposition_ten(booknum))
+def test_if_13_true
+  assert_equal(true, check_length("1234567890123"))
 end
+
+def test_if_more_than_13_false
+  assert_equal(false, check_length("12345678901234"))
+end
+
+def test_if_a_13number_is_false
+  assert_equal(false, check_isbn_13(978-0-262-13472-9))
+end
+
+def test_13number_tests
+  assert_equal(true, check_isbn_13(978-3-16-148410-0))
+end
+
+
+
+
+
+
+
+
+
 
 end
