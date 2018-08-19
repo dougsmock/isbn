4107b12a9bff188260a208c194c9baf464036c15
Your Name <you@example.com>
2018-08-19 16:25:39 -0400

    Think I have solved 10- and 13-digit calculation.

--- isbn_numbers.rb
+++ isbn_numbers.rb
@@ -1,3 +1,26 @@
+
+  # if arr.pop.downcase == "x"
+  #   isbn_check = 10
+  # else
+  #   isbn_check = isbn_check.to_i
+  # end
+  #
+  # isbn_sum = 0
+  # arr.each_with_index do |v, i|
+  #   unless v.to_i.to_s == v
+  #     return false
+  #   end
+  # isbn_sum += v.to_i * (i+1)
+  # end
+  #
+  # if isbn_sum % 11 == isbn_check
+  #   return true
+  # else
+  #   return false
+  # end
+############ Justin's code above
+
+
 # def book_long(booknum) #changes string to array of one char each; eliminates hyphens and spacesvalidates length
 #   p "1 book long called."
 #   book_arr = booknum.each_char.to_a
@@ -62,34 +85,34 @@
 #   booknum
 # end
 #
-# def math_ten(booknum) #function to do 10-number math
-#   p "5/ math 10 called"
-#   book_arr = xposition_ten(booknum)
-#
-#   book_int = []
-#   j = 0
-#   while j < 9
-#     book_int[j] = book_arr[j].to_i
-#     j += 1
-#   end
-#
-#   book_int << book_arr[9]
-#   # p book_int
-#
-#   checkmath = (book_int[0] * 1) + (book_int[1] * 2) + (book_int[2] * 3) + (book_int[3] * 4) + (book_int[4] * 5) + (book_int[5] * 6) + (book_int[6] * 7) + (book_int[7] * 8) + (book_int[8] * 9)
-#   checkdig_check = checkmath % 11
-#
-#   # p checkmath
-#   # p checkdig_check
-#
-#   if (checkdig_check == 10 && book_arr[9] == "X") || (checkdig_check == 10 && book_arr[9] == "x")
-#     p "Number is valid!"
-#   elsif checkdig_check == book_arr[9]
-#     p "Number is valid!"
-#   else
-#     p "Number is invalid."
-#   end
-# end
+def math_ten(booknum) #function to do 10-number math
+  p "5/ math 10 called"
+  book_arr = xposition_ten(booknum)
+
+  book_int = []
+  j = 0
+  while j < 9
+    book_int[j] = book_arr[j].to_i
+    j += 1
+  end
+
+  book_int << book_arr[9]
+  # p book_int
+
+  checkmath = (book_int[0] * 1) + (book_int[1] * 2) + (book_int[2] * 3) + (book_int[3] * 4) + (book_int[4] * 5) + (book_int[5] * 6) + (book_int[6] * 7) + (book_int[7] * 8) + (book_int[8] * 9)
+  checkdig_check = checkmath % 11
+
+  # p checkmath
+  # p checkdig_check
+
+  if (checkdig_check == 10 && book_arr[9] == "X") || (checkdig_check == 10 && book_arr[9] == "x")
+    p "Number is valid!"
+  elsif checkdig_check == book_arr[9]
+    p "Number is valid!"
+  else
+    p "Number is invalid."
+  end
+end
 #
 # def math_13(booknum) #function to do 13-number math
 #   p "6/ math 13 called."
@@ -123,6 +146,7 @@
 def remove_hyphens_spaces(isbn)
   arr = isbn.split("")
   arr.delete_if {|c| c==" " || c=="-"}
+  arr
   end
 
 def check_length(isbn)
@@ -151,24 +175,31 @@ def invalid_char(isbn)
     end
 end
 
-def math_10(isbn)
+def math_10(isbn) # do we need a no-x function?
   arr = isbn.split("")
-
-  if arr.pop == "x" || arr.pop == "X"
-    isbn_check = 10
-  else
-    isbn_check = isbn_check.to_i
-  end
-
+  j = 0
   isbn_sum = 0
-  arr.each_index do |v, i|
-    unless v.to_i.to_s == v
-      return false
-    end
-  isbn_sum += v.to_i * (i+1)
+  while j < 9
+    arr[j] = arr[j].to_i #makes number 0-8 an integer
+    isbn_sum += arr[j] * (j + 1) # does the math
+    j += 1
   end
 
-  if isbn_sum % 11 == isbn_check
+  if arr[9].downcase != "x"
+    check_dig = arr[9].to_i
+  elsif arr[9].downcase == "x"
+    check_dig = "x"
+  end
+
+  mod = isbn_sum % 11
+  if mod == 10
+    mod = "x"
+  end
+
+    # p check_dig
+    # p mod
+
+  if check_dig == mod
     return true
   else
     return false
@@ -176,20 +207,28 @@ def math_10(isbn)
 end
 
 
+
 def math_13(isbn)
   arr = isbn.split("")
-  isbn.delete {|c| c==" " || c=="-"}
     i = 0
   while i < 13 do
-    isbn[i].to_i
+    arr[i] = arr[i].to_i
     i += 1
   end
 
-  isbn_sum = isbn[0] + (isbn[1] * 3) + isbn[2] + (isbn[3] * 3) + isbn[4] + (isbn[5] * 3) + isbn[6] + (isbn[7] * 3) + isbn[8] + (isbn[9] * 3) + isbn[10] + (isbn[11] * 3)
+  isbn_sum = 0
+  isbn_sum = arr[0] + (arr[1] * 3) + arr[2] + (arr[3] * 3) + arr[4] + (arr[5] * 3) + arr[6] + (arr[7] * 3) + arr[8] + (arr[9] * 3) + arr[10] + (arr[11] * 3)
 
-  isbn_mod = isbn_sum % 10
-  isbn_check = 10 - isbn_mod
+  check_dig = arr[12]
 
-  isbn_check == isbn[12]
+  mod = 10 - (isbn_sum % 10)
+    # if mod == 10
+    #   mod = 0
+    # end
 
+  if check_dig = mod
+    return true
+  else
+    return false
+  end
 end
--- isbn_tdd.rb
+++ isbn_tdd.rb
@@ -3,74 +3,13 @@ require_relative "isbn_numbers.rb"
 
 class HashSize < Minitest::Test
 
-# def test_assert_1_equals_1
-#   assert_equal(1, 1)
-# end
-#
-# def test_is_string_13_char
-#   booknum = "8933789896890"
-#   assert_equal(13, booknum.size)
-# end
-#
-# # def test_is_string_10_char #above test and this test cannot pass at same time with same string
-# #   booknum = "8933789896890"
-# #   assert_equal(10, booknum.size)
-# # end
-#
-# def test_string_no_without_spaces
-#   booknum = "8933789896890"
-#   assert_equal(13, booknum.size)
-# end
-#
-# def test_string_no_with_hyphens_and_spaces() #... test deals with 13-char numbers
-#   book_arr = "89337-8989-68 90"
-#   assert_equal(13, book_long(book_arr))
-# end
-#
-# def test_if_letters_are_invalid_in_10_chars #... passed with weird failure
-#   book_arr = "12-3456 78-9X"
-#   assert_equal(true, invalid_char_ten(book_arr))
-# end
-#
-# def test_if_x_is_in_right_place
-#   booknum = "12-3456 78-9X"
-#   assert_equal("X is in right place!", xposition_ten(booknum))
-# end
-#
-# # def test_if_x_is_in_wrong_place #draws an invalid message, no count of failures, etc.
-# #   booknum = "12-X456 78-93"
-# #   assert_equal("X is in the right place!", xposition_ten(booknum))
-# # end
-#
-# def test_if_booknum_is_valid_or_not
-#   booknum = "1234 5678 9X"
-#   assert_equal("Number is valid!", math_ten(booknum))
-# end
-#
-# def test_that_booknum_is_invalid
-#   booknum = "1234 5678 90"
-#   assert_equal("Number is invalid.", math_ten(booknum))
-# end
-#
-# def test_that_booknum_is_invalid_by_X_position
-#   booknum = "1234 X678 90"
-#   assert_equal("Invalid number; X in improper position.", math_ten(booknum))
-# end
-#
-# def test_that_booknum_is_invalid_by_another_letter
-#   booknum = "1234 0678 9090 8"
-#   assert_equal("Entry invalid; illegal character.", invalid_char_ten(booknum))
+# def test_if_hyphens_spaces_out10
+#   assert_equal(["1", "2", "3", "4", "5", "6", "7", "8", "9", "X"], math_10("123-45-6789 X"))
 # end
 
-#refactor ///////////////
-
-def test_if_hyphens_spaces_out10
-  assert_equal(["1", "2", "3", "4", "5", "6", "7", "8", "9", "X"], remove_hyphens_spaces("123-45-6789 X"))
-end
-
-def test_if_hyphens_spaces_out10
-  assert_equal(["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "1", "2", "3"], remove_hyphens_spaces("123-45-6789 0123"))
-end
+# def test_if_hyphens_spaces_out10
+#   assert_equal(["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "1", "2", "3"], math_13("123-45-6789 0123"))
+# end
 
 def test_if_length_is_10
   assert_equal(true, check_length("123456789X"))
@@ -84,33 +23,20 @@ def test_if_bad_char_invalid
   assert_nil(nil, invalid_char("0471A58697"))
 end
 
+def test_if_13_check_length
+  assert_equal(true, check_length("1234567890123"))
+end
+
+def test_if_more_than_13_false
+  assert_equal(false, check_length("12345678901234"))
+end
+
 def test_if_number_is_valid_10
-  assert_equal(true, math_10("0198526636"))
+  assert_equal(true, math_10("1861972717"))
 end
 
 def test_if_number_is_valid_13
-  assert_equal(true, math_13("978-0-262-13472-9"))
+  assert_equal(true, math_13("9780262134729"))
 end
-#
-# def test_if_13_check_length
-#   assert_equal(true, check_length("1234567890123"))
-# end
-#
-# def test_if_more_than_13_false
-#   assert_equal(false, check_length("12345678901234"))
-# end
-#
-# def test_if_a_13number_is_false
-#   assert_equal(false, check_isbn_13(978-0-262-13472-9))
-# end
-#
-# def test_if_10_number_is_true
-#   assert_equal(true, check_isbn("123456789X"))
-# end
-#
-# def test_13number_tests
-#   assert_equal(true, check_isbn_13("978-3-16-148410-0")) # cannot get it to pass.
-# end
-
 
 end
